167e62ff8cb0807464d8455e3bd694a2057e4ae3
Laura Catalina Hernández Rodríguez <lauraherrod@unisabana.edu.co>
2025-10-24 13:53:22 -0500

    Second frontend deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Start from "./pages/start.jsx";
+import start from "./pages/start.jsx";
 import LogIn from "./pages/LogIn.jsx";
 import DriverSignIn from "./pages/SignIn/DriverSignIn.jsx";
 import PassengerSignIn from "./pages/SignIn/PassengerSignIn.jsx";
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/start" replace />} />
-        <Route path="/start" element={<Start />} />
+        <Route path="/start" element={<start />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/driverSignIn" element={<DriverSignIn />} />
         <Route path="/passengerSignIn" element={<PassengerSignIn />} />
