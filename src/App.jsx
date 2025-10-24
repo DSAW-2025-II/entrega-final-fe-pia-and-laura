e393c96cd1b3fa72950b5151eb2beed1978f7901
Laura Catalina Hernández Rodríguez <lauraherrod@unisabana.edu.co>
2025-10-24 13:43:58 -0500

    for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Start from "./pages/Start";
-import LogIn from "./pages/LogIn";
-import DriverSignIn from "./pages/DriverSignIn";
-import PassengerSignIn from "./pages/PassengerSignIn";
+import Start from "./pages/start.jsx";
+import LogIn from "./pages/LogIn.jsx";
+import DriverSignIn from "./pages/SignIn/DriverSignIn.jsx";
+import PassengerSignIn from "./pages/SignIn/PassengerSignIn.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 🔹 Redirección de la raíz "/" hacia "/start" */}
         <Route path="/" element={<Navigate to="/start" replace />} />
-
-        {/* 🔹 Rutas principales */}
         <Route path="/start" element={<Start />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/driverSignIn" element={<DriverSignIn />} />
         <Route path="/passengerSignIn" element={<PassengerSignIn />} />
-
-        {/* 🔹 Página por defecto si no hay coincidencia */}
         <Route path="*" element={<Navigate to="/start" replace />} />
       </Routes>
     </BrowserRouter>
