16ea9b83253c0886b300a961378204acc30a4897
Laura Catalina Hernández Rodríguez <lauraherrod@unisabana.edu.co>
2025-11-11 17:44:30 -0500

    Fix on maps

--- src/pages/components/SearchRideMap.jsx
+++ src/pages/components/SearchRideMap.jsx
@@ -3,24 +3,25 @@ import Map, { Marker, NavigationControl } from "react-map-gl";
 
 export default function SearchRideMap() {
   const [viewState, setViewState] = useState({
-    longitude: -74.0325, // Chía, Universidad de La Sabana
+    longitude: -74.0325,
     latitude: 4.8616,
     zoom: 14,
   });
-    console.log("TOKEN:", import.meta.env.VITE_MAPBOX_TOKEN);
+
+  // Guarda el token explícitamente en una constante
+  const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+
+  console.log("TOKEN MAPBOX:", MAPBOX_TOKEN);
 
   return (
     <div className="relative w-full h-[600px] rounded-2xl overflow-hidden shadow-lg">
       <Map
         {...viewState}
-        onMove={evt => setViewState(evt.viewState)}
-        mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+        onMove={(evt) => setViewState(evt.viewState)}
+        mapboxAccessToken={MAPBOX_TOKEN}
         mapStyle="mapbox://styles/mapbox/streets-v12"
       >
-        {/* Marcador principal */}
         <Marker longitude={-74.0325} latitude={4.8616} color="#1F2937" />
-
-        {/* Controles (zoom, rotación, etc.) */}
         <NavigationControl position="top-right" />
       </Map>
     </div>
